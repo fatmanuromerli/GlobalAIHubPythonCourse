62d6c985106930a575c607d8bfe49b1e49b2bc62
fatmanuromerli <79212227+fatmanuromerli@users.noreply.github.com>
2021-04-11 13:50:26 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,68 @@
-#Explain your work
+ogrenciler = {}
+ogrSayisi = int(input("Kaç öğrenci bilgisi gireceksiniz (önerilen 5): "))
+count = 0
+while True:
+    kacinciOgr = input("Kaçıncı öğrenciyi giriyorsunuz (örneğin öğrenci1 - öğrenci2) : ")
+    ogrAd = input("Öğrenci adını giriniz : ")
+    ogrSoyad = input("Öğrenci soyadını giriniz : ")
+    ogrNum = int(input("Öğrenci numarasını giriniz : "))
+    midterm = int(input("Öğrenci midterm notunu giriniz : "))
+    project = int(input("Öğrenci project notunu giriniz : "))
+    final = int(input("Öğrenci final notunu giriniz : "))
+    ogrenciler.update({
+        kacinciOgr: {
+            "ad": ogrAd,
+            "soyad": ogrSoyad,
+            "ogrenciNum": ogrNum,
+            "midterm": midterm,
+            "project": project,
+            "final": final
+        }
+    })
+    count += 1
 
-#Question 1
-for x in range(a):
-	print(a)
+    if count == ogrSayisi:
+        break
+
+
+print(ogrenciler)
+
+
+notSozluk = {}
+ogrBilgi = ogrenciler.values()
+isimlist = []
+soyadlist = []
+numlist = []
+passingGradelist = []
+sayac = 0
+for i in ogrBilgi:
+    passingGrade = i["midterm"] * (0.3) + i["project"] * (0.3) + i["final"] * (0.4)
+    passingGradelist.append(passingGrade)
+    isimlist.append(i["ad"])
+    soyadlist.append(i["soyad"])
+    numlist.append(i["ogrenciNum"])
+
+
+sonList = list(zip(isimlist, soyadlist, numlist, passingGradelist))
+
+grades = []
+for fno in sonList:
+    grades.append(fno[3])
+grades = sorted(grades, reverse=True)
+uzunluk = len(grades)
+print("Ad\t\t\t\t\tSoyad\t\t\t\tNum\t\t\t\tPassingGrade\n")
+while uzunluk > 0:
+
+    for satir in sonList:
+        if uzunluk == 0:
+            break
+        if grades[0] == satir[3]:
+
+            for indexs in satir:
+                print(str(indexs), end='\t\t\t')
+            print()
+            grades.remove(grades[0])
+            uzunluk -= 1
+
+
+print("Sıralanmış öğrenci listesi : ", sonList)
