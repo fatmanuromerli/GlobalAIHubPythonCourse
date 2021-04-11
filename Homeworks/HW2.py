99b2f5f348858f0c1b7ec787e2aba657ca5e79a2
fatmanuromerli <79212227+fatmanuromerli@users.noreply.github.com>
2021-04-11 13:49:38 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,28 @@
-#Explain your work
+#day3 
+oncedenBelirlenmisKullaniciAdi = input("bir kullanıcı adı belirleyiniz: ")
+oncedenBelirlenmisSifre = input("bir şifre belirleyiniz : ")
 
-#Question 1
-for x in range(a):
-	print(a)
+
+kullaniciBilgileri = {
+    "kullaniciAdi" : oncedenBelirlenmisKullaniciAdi,
+    "sifre" : oncedenBelirlenmisSifre
+
+}
+
+
+while True:
+    kullaniciAdiKontrol = input("Önceden belirlediğiniz kullanıcı adınızı giriniz: ")
+    sifreKontrol = input("Önceden belirlediğiniz şifrenizi giriniz: ")
+
+    if kullaniciAdiKontrol == kullaniciBilgileri["kullaniciAdi"] :
+        if sifreKontrol == kullaniciBilgileri["sifre"]:
+            print("Giriş başarılı")
+            break
+        else:
+            print("HATALI ŞİFRE. LÜTFEN TEKRAR DENEYİNİZ.")
+            continue
+    else:
+        print("HATALI KULLANICI ADI. LÜTFEN TEKRAR DENEYİNİZ.")
+        continue
+
+
